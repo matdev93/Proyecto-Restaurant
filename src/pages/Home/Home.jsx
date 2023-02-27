a629e3cecf8cd83a8cea11a9f01c364cf94a74ec
matdev93 <112671782+matdev93@users.noreply.github.com>
2023-02-27 01:31:00 -0300

    Ok Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -48,6 +48,17 @@ const Home = () => {
           <Button variant="warning">Go somewhere</Button>
         </Card.Body>
       </Card>
+      <Card style={{ width: '18rem' , height: '20rem' }}>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1579306194872-64…lfHx8fGVufDB8fHx8&auto=format&fit=crop&w=817&q=80" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="warning">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   </div>
   )
