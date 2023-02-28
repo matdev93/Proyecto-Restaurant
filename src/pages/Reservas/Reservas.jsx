3259abe952145ad7092d2a5b999b2c89bf92747b
matdev93 <112671782+matdev93@users.noreply.github.com>
2023-02-28 01:08:49 -0300

    Avances diseño

--- src/pages/Reservas/Reservas.jsx
+++ src/pages/Reservas/Reservas.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect } from 'react';
+import { db } from '../../config/Firebase';
+import { collection, getDocs, addDoc} from 'firebase/firestore';
+import Tarjetareserva from  '../../components/Tarjetareserva'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const Reservas = () => {
   return (
